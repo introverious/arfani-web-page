eb775f1b34229c482ae57f3749dc0b193db18a37
introverious <147398651+introverious@users.noreply.github.com>
2024-10-08 16:55:24 +0800

    Add files via upload

--- assets/resources/index.js
+++ assets/resources/index.js
@@ -1,9 +1,10 @@
-// Button AJAX
-// document.getElementById("ajaxButton").addEventListener("click", function () {
-//   // AJAX call logic here
-//   alert("AJAX call initiated!");
-// });
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    document.getElementById("menu-toggle").checked = false;
+  }
+});
 
+// Slide Show Functionality
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -32,9 +33,36 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
+    dots[slideIndex - 1].className += " active";
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+// Initialize EmailJS
+(function () {
+  emailjs.init("ylxAamj6lPGpxACme"); // Replace 'YOUR_USER_ID' with your actual EmailJS user ID
+})();
+
+function sendMail() {
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send("service_tgm9yo8", "template_1wa2ny9", params).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Email sent successfully!");
+      document.getElementById("contactForm").reset();
+    },
+    function (error) {
+      console.log("FAILED...", error);
+      alert("Failed to send email. Please try again later.");
+    }
+  );
 }
